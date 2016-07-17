5e99f27d036ca0dd97704213e9b70ddb097ecd01
Gillian BC <gbc@tesco.net>
2016-07-17 20:32:16 +0100

    Changed package name of facade example

---
+++ stuff/src/com/gill/facadepattern/Shape.java
@@ -0,0 +1,5 @@
+package com.gill.facadepattern;
+
+public interface Shape {
+	void draw();
+}
---
+++ stuff/src/com/gill/facadepattern/Square.java
@@ -0,0 +1,11 @@
+package com.gill.facadepattern;
+
+public class Square implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println(this.getClass().getName() + " drawn");
+		
+	}
+
+}
