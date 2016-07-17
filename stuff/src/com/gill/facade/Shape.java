4b0802029524f82e0466bff37d1fad48ad03792a
Gillian BC <gbc@tesco.net>
2016-07-17 20:28:04 +0100

    Added facade example

---
+++ stuff/src/com/gill/facade/Shape.java
@@ -0,0 +1,5 @@
+package com.gill.facade;
+
+public interface Shape {
+	void draw();
+}
---
+++ stuff/src/com/gill/facade/Square.java
@@ -0,0 +1,11 @@
+package com.gill.facade;
+
+public class Square implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println(this.getClass().getName() + " drawn");
+		
+	}
+
+}
